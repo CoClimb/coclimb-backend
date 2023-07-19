6348cd6d4c3671c5db406485cb9500cef043fb10
JJBINY <97151887+JJBINY@users.noreply.github.com>
2023-07-19 20:09:46 +0900

    Revert "패키지 구조 동기화"

--- src/main/java/swm/s3/coclimb/api/application/port/in/gym/GymQuery.java
+++
@@ -1,9 +0,0 @@
-package swm.s3.coclimb.api.application.port.in.gym;
-
-import java.util.List;
-
-public interface GymQuery {
-    GymInfoResponseDto getGymInfoByName(String name);
-
-    List<GymLocationResponseDto> getGymLocations();
-}
